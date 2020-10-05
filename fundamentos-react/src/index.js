da0ba5da01df402d2a4295cc8dc20df74e34a47f
Roberto <roberto.rosa7@gmail.com>
2020-10-05 19:30:26 -0300

    primeiro component

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,8 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+const tag = <strong>ReactJS</strong>
+
+
+ReactDOM.render(<div>{tag}</div>, document.querySelector('#root'))
