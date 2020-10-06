7d7bc4b59c10874cf7b44476861dac8f83b8e467
Roberto <roberto.rosa7@gmail.com>
2020-10-06 10:57:13 -0300

    dados de usuario

--- fundamentos-react/src/components/basics/ComParametro.jsx
+++ fundamentos-react/src/components/basics/ComParametro.jsx
@@ -1,10 +1,29 @@
+import './ComParametro.css'
 import React from 'react'
+
+
 export default function ComParamentro(props) {
   return (
-    <section>
-      <h1>Component com paramentro</h1>
-      <p>{props.titulo}</p>
-      <p>{props.subtitulo}</p>
+    <section className="com-parametro">
+      <h1>Component com paramentro - dados de aluno</h1>
+      <div className="card">
+        <p><span>Nome: </span>{props.user.nome}</p>
+        <p><span>Sobrenome: </span>{props.user.sobrenome}</p>
+        <p><span>Apelido: </span>{props.user.apelido}</p>
+        <p><span>Idade: </span>{props.user.idade}</p>
+        <p><span>Sexo: </span>{props.user.sexo}</p>
+        <p><span>Universidade: </span>{props.user.instituicao_ensino}</p>
+        <p><span>Profissão: </span>{props.user.profissao}</p>
+        <div className="media-social">
+          {
+            props.user.media_social.map((element, index) => {
+              return (
+                <a href={element.url} key={index}>{element.tipo}</a>
+              )
+            })
+          }
+        </div>
+      </div>
     </section>
   )
 }
--- fundamentos-react/src/index.jsx
+++ fundamentos-react/src/index.jsx
@@ -6,9 +6,43 @@ import React from 'react'
 import ComParametro from './components/basics/ComParametro'
 import Primeiro from './components/basics/Primeiro'
 
+const user = {
+  "nome":"Roberto",
+  "sobrenome": "Rosa da Silva",
+  "apelido":"beto",
+  "idade": 32,
+  "sexo": "Masculino",
+  "instituicao_ensino":"Universidade nove de Julhos",
+  "altura": 1.75,
+  "cor": "Branco",
+  "biotipo":"Magro",
+  "profissao":"Desenvolvedor de Software",
+  "foto":"#",
+  "media_social": [
+    {
+      "tipo":"facebook",
+      "following":0,
+      "follower":0,
+      "url":"#"
+    },
+    {
+      "tipo":"twitter",
+      "following":0,
+      "follower":0,
+      "url":"#"
+    },
+    {
+      "tipo":"instagram",
+      "following":0,
+      "follower":0,
+      "url":"#"
+    }
+  ]
+}
+
 ReactDOM.render(
   <main>
     <Primeiro />
-    <ComParametro titulo="Situação do aluno" nota={3.9} nome="João" />
+    <ComParametro user={user} />
   </main>,
   document.querySelector('#root'))
