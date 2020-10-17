bce9213f14e9249e9c0fcce9eed411ca83181519
Roberto <roberto.rosa7@gmail.com>
2020-10-17 10:23:01 -0300

    mega

--- fundamentos-react/src/components/mega/Mega.jsx
+++ fundamentos-react/src/components/mega/Mega.jsx
@@ -1,13 +1,72 @@
 import React from "react";
 
 export default class Mega extends React.Component {
+  pStyles = {
+    width: '100%',
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent:'space-between'
+  }
+  numberLuck = {
+    display:'inline-block',
+    width: '40px',
+    height: '40px',
+    padding: '8px',
+    background: '#0f0',
+    borderRadius: '50%',
+    margin:'auto',
+    fontWeight: '700'
+  }
   constructor(props) {
     super(props);
+    this.state = {
+      result_sorteio: this.otherWaytoGenerateArray(this.props.qtde || 6),
+    };
+    // const arr = [];
+
+    // for (let i = 0; i < 10; i++) {
+    //   arr.push(this.createNumArray(1, 61));
+    // }
+    // console.log(arr);
   }
 
-  gerarNumNaoContido = (min, max, array) => {
-    const random = parseInt(Math.random() * (max - min)) + min;
-    return array.includes(random) ? this.gerarNumNaoContido(min, max, array) : random;
+  setResultSorteio = () => {
+    this.setState({ result_sorteio: this.otherWaytoGenerateArray(6) });
   };
-  render = () => <div>mega</div>;
+
+  // createNumArray = (min, max) => {
+  //   const arr = [];
+  //   for (let i = 0; i < 6; i++) {
+  //     arr.push(this.gerarNumNaoContido(min, max, arr));
+  //   }
+  //   return arr;
+  // };
+
+  otherWaytoGenerateArray = (num) => {
+    return Array(num)
+      .fill(0)
+      .reduce((n) => [...n, this.gerarNumNaoContido(1, 61, n)], [])
+      .sort((a, b) => a - b);
+  };
+
+  gerarNumNaoContido = (min, max, array) => {
+    const random = Math.ceil(Math.random() * (max - min) + min);
+    return array.includes(random)
+      ? this.gerarNumNaoContido(min, max, array)
+      : random;
+  };
+  render = () => (
+    <div>
+      <p style={this.pStyles}>
+        {this.state.result_sorteio.map((value, index) => (
+          <span style={this.numberLuck} key={index}>{value}</span>
+        ))}
+      </p>
+      <div className="container-btn">
+        <button className="btn" type="button" onClick={this.setResultSorteio}>
+          Mega Sena Resultado
+        </button>
+      </div>
+    </div>
+  );
 }
