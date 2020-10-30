28826b2f7433e107810fe3d64f7bf7742204d705
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-10-30 10:35:28 -0300

    app todo - nodejs settings

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # React
+
+###### 30/10/2020
+[x] Backend App Todo
+[x] NodeJs settings Server
+  + Middlewares
+  + Starting Server
---
+++ app_todo/backend/src/config/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 3003;
+
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.listen(PORT, () => console.log(`The server is run on: ${PORT}`));
