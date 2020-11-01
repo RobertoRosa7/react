5de331db9cdf81678f5359d2ea466117cc28db22
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-01 13:56:14 -0300

    adicionando cors na aplicaçao

--- app_todo/backend/src/config/server.js
+++ app_todo/backend/src/config/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 3003;
-
+const cors = require("./cors");
 const server = express();
 
+server.use(cors);
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.listen(PORT, () => console.log(`The server is run on: ${PORT}`));
 
-module.exports = server
+module.exports = server;
