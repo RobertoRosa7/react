594a3a09fc8d55d1cc726ac5b7c2a567477ccd46
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-01 13:28:27 -0300

    configurando routes

--- app_todo/backend/src/config/server.js
+++ app_todo/backend/src/config/server.js
@@ -7,3 +7,5 @@ const server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.listen(PORT, () => console.log(`The server is run on: ${PORT}`));
+
+module.exports = server
