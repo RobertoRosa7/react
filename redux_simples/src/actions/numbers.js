2c47d30eced9dabe90c04668eb2c5a6d5473f627
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-06 13:11:04 -0300

    export const for actions types

--- redux_simples/src/actions/numbers.js
+++ redux_simples/src/actions/numbers.js
@@ -1,30 +1,16 @@
 // Action Creator
-
-// export const actions = {
-//   changeNumMin: function (n) {
-//     return {
-//       type: "MIN_NUMBER_CHANGED",
-//       payload: n,
-//     };
-//   },
-//   changeNumMax: function (n) {
-//     return {
-//       type: "MAX_NUMBER_CHANGED",
-//       payload: n,
-//     };
-//   },
-// };
+import { MAX_NUMBER_CHANGED, MIN_NUMBER_CHANGED } from "./actions-types";
 
 export function changeNumMin(n) {
   return {
-    type: "MIN_NUMBER_CHANGED",
+    type: MIN_NUMBER_CHANGED,
     payload: n,
   };
 }
 
 export function changeNumMax(n) {
   return {
-    type: "MAX_NUMBER_CHANGED",
+    type: MAX_NUMBER_CHANGED,
     payload: n,
   };
 }
