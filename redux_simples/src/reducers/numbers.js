37e8c7386d58d3f698f62e9701e04c9c7aa97370
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-06 13:13:06 -0300

    export const for reducers types

--- redux_simples/src/reducers/numbers.js
+++ redux_simples/src/reducers/numbers.js
@@ -1,15 +1,20 @@
+import {
+  MAX_NUMBER_CHANGED,
+  MIN_NUMBER_CHANGED,
+} from "../actions/actions-types";
+
 const initialState = {
   min: 1,
   max: 10,
 };
 export default function reduxNumbers(state = initialState, action) {
   switch (action.type) {
-    case "MIN_NUMBER_CHANGED":
+    case MIN_NUMBER_CHANGED:
       return {
         ...state,
         min: action.payload,
       };
-    case "MAX_NUMBER_CHANGED":
+    case MAX_NUMBER_CHANGED:
       return {
         ...state,
         max: action.payload,
