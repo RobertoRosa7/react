c018c155db4ece069b3fbcee5ecd5dcd8aa0a290
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-10 20:26:21 -0300

    implementaçao de redux na aplicaçao

--- app_todo/frontend/src/index.jsx
+++ app_todo/frontend/src/index.jsx
@@ -4,9 +4,11 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./main/app";
-import TodoStore from './store/todo.store'
+import TodoStore from "./store/todo.store";
 
-const store = createStore(TodoStore);
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(TodoStore, devTools);
 ReactDom.render(
   <Provider store={store}>
     <App />
