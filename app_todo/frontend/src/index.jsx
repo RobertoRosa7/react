8cb2293076a99a17826af99f28ba763c99a0223d
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-10 10:48:17 -0300

    rename name of files

--- app_todo/frontend/src/index.jsx
+++ app_todo/frontend/src/index.jsx
@@ -4,7 +4,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./main/app";
-import reducers from "./reducers/reducers";
+import reducers from "./reducers/todo.reducers";
 
 const store = createStore(reducers);
 ReactDom.render(
