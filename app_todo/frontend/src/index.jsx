4f97a1d45f6b7a16e879102db3417c2acbbde492
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-11 20:40:53 -0300

    aplicando e configurando middleware redux-promise

--- app_todo/frontend/src/index.jsx
+++ app_todo/frontend/src/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import promise from "redux-promise";
 
 import App from "./main/app";
 import TodoStore from "./store/todo.store";
 
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(TodoStore, devTools);
+const store = applyMiddleware(promise)(createStore)(TodoStore, devTools);
 ReactDom.render(
   <Provider store={store}>
     <App />
