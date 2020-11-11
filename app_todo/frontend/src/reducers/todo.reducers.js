da45ebe8c4968d735b413adb0d6e26ddc03513a7
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-11 20:44:45 -0300

    clear initial state reducers default

--- app_todo/frontend/src/reducers/todo.reducers.js
+++ app_todo/frontend/src/reducers/todo.reducers.js
@@ -1,22 +1,6 @@
 const INITIAL_STATE = {
-  description: "Ler livro",
-  list: [
-    {
-      _id: 1,
-      description: "Pagar pensão",
-      done: true,
-    },
-    {
-      _id: 2,
-      description: "Pagar fatura do cartão nubank",
-      done: false,
-    },
-    {
-      _id: 3,
-      description: "Pagar curso de inglês",
-      done: false,
-    },
-  ],
+  description: "",
+  list: [],
 };
 
 export default function TodoReducers(state = INITIAL_STATE, actions) {
