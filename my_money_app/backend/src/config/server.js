74ecb8c3284cb7ec4eabb1476f6145b8e9b7b8af
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-16 17:13:53 -0300

    pagination

--- my_money_app/backend/src/config/server.js
+++ my_money_app/backend/src/config/server.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3003;
 const cors = require("./cors");
+const queryParser = require("express-query-int");
 
 app.use(cors);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(queryParser);
 
 app.listen(PORT, () => console.log(`Server is running on: ${PORT}`));
 
