0900e57e6d2083dcebad47eb517f3b8c153959db
RobertoRosa7 <roberto.rosa7@gmail.com>
2020-11-16 15:29:13 -0300

    mapeamento de rotas

--- my_money_app/backend/src/api/billingCycle/billingCycle.js
+++ my_money_app/backend/src/api/billingCycle/billingCycle.js
@@ -2,13 +2,21 @@ const restfull = require("node-restful");
 const mongoose = restfull.mongoose;
 
 const creditSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  name: { type: String, required: [true, "Report the credit's name."] },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Report the value of credit."],
+  },
 });
 
 const debitSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  name: { type: String, required: [true, "Report the debit's name."] },
+  value: {
+    type: Number,
+    min: 0,
+    required: [true, "Report the value of debit."],
+  },
   status: {
     type: String,
     required: false,
@@ -18,9 +26,14 @@ const debitSchema = new mongoose.Schema({
 });
 
 const billingCycleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  month: { type, Number, min: 1, max: 12, required: true },
-  year: { type: Number, min: 1970, max: 2100, required: true },
+  name: { type: String, required: [true, "Report operation name."] },
+  month: { type: Number, min: 1, max: 12, required: [true, "Report month."] },
+  year: {
+    type: Number,
+    min: 1970,
+    max: 2100,
+    required: [true, "Report year."],
+  },
   credits: [creditSchema],
   debits: [debitSchema],
 });
