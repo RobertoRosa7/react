b42e5ad6bab1a02636aa872680e55215035c8fbb
Roberto Rosa <roberto.rosa7@gmail.com>
2021-01-10 10:47:36 -0300

    fixing errors of names and directories

--- my_money_app/frontend/src/common/template/dependencies.js
+++ my_money_app/frontend/src/common/template/dependencies.js
@@ -1,5 +1,5 @@
 import 'modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
-import 'modules/admin-lte/plguins/fastclick/fastclick'
+import 'modules/admin-lte/plugins/fastclick/fastclick'
 import 'modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
 import 'modules/admin-lte/dist/js/app.min'
 
